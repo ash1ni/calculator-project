c945294c81bb2b95481b393a167941f1da5d9350
stonks <ash1ni>
2021-12-11 00:37:01 +0530

    updated some features

--- script.js
+++ script.js
@@ -23,37 +23,44 @@ class Calculator {
     chooseOperation(operation) {
       if (this.currentOperand === '') return
       if (this.previousOperand !== '') {
-        this.compute()
+         this.compute()
+
       }
       this.operation = operation
       this.previousOperand = this.currentOperand
       this.currentOperand = ''
     }
-  
+    
     compute() {
       let computation
       const prev = parseFloat(this.previousOperand)
       const current = parseFloat(this.currentOperand)
       if (isNaN(prev) || isNaN(current)) return
+
+      
+    
       switch (this.operation) {
+        
+        case '÷':
+          computation = prev / current
+          break
+        case '*':
+          computation = prev * current
+          break
         case '+':
           computation = prev + current
           break
         case '-':
           computation = prev - current
           break
-        case '*':
-          computation = prev * current
-          break
-        case '÷':
-          computation = prev / current
-          break
         default:
           return
       }
+      
       this.currentOperand = computation
       this.operation = undefined
       this.previousOperand = ''
+      
     }
   
     getDisplayNumber(number) {
@@ -81,11 +88,10 @@ class Calculator {
           `${this.getDisplayNumber(this.previousOperand)} ${this.operation}`
       } else {
         this.previousOperandTextElement.innerText = ''
+        
       }
     }
   }
-  
-  
   const numberButtons = document.querySelectorAll('[data-number]')
   const operationButtons = document.querySelectorAll('[data-operation]')
   const equalsButton = document.querySelector('[data-equals]')
@@ -93,6 +99,10 @@ class Calculator {
   const allClearButton = document.querySelector('[data-all-clear]')
   const previousOperandTextElement = document.querySelector('[data-previous-operand]')
   const currentOperandTextElement = document.querySelector('[data-current-operand]')
+  let screen= document.getElementById('ans'); 
+  buttons = document.querySelectorAll('button');
+  
+  
   
   const calculator = new Calculator(previousOperandTextElement, currentOperandTextElement)
   
@@ -124,3 +134,8 @@ class Calculator {
     calculator.delete()
     calculator.updateDisplay()
   })
+  
+  
+//let screen= document.getElementById('ans'); 
+//buttons = document.querySelectorAll('button');
+  
